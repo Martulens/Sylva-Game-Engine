960dce5af14bf0e5fa89679df150e8aa4a83f498
Martulens <153864504+Martulens@users.noreply.github.com>
2026-01-10 00:59:14 +0100

    Delete src/cmake-build-debug directory

--- src/cmake-build-debug/_deps/assimp-build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/martul3ns/projects/OpenGL-Game-Engine/src/cmake-build-debug/_deps/assimp-src/contrib/zlib
-# Build directory: /home/martul3ns/projects/OpenGL-Game-Engine/src/cmake-build-debug/_deps/assimp-build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
